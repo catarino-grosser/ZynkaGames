e37ab432752ccf577286bd69a5b1174db4b76082
catarino-grosser <catarinogrosser@gmail.com>
2026-07-07 21:02:15 -0300

    update

--- aventuras/index.js
+++
@@ -1,9 +0,0 @@
-import reinoPerdido from './reino-perdido.js';
-import cidadeDosMortos from './cidade-dos-mortos.js';
-import missaoMarte from './missao-marte.js';
-
-export const localAdventures = [
-  reinoPerdido,
-  cidadeDosMortos,
-  missaoMarte
-];
